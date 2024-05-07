2839bb09a96bb718ef046de0c276bc082b65c65a
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2024-05-07 17:25:38 +0530

    added space for initial test

--- designPatterns/behavioral01_chain_of_responsibility.cpp
+++ designPatterns/behavioral01_chain_of_responsibility.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * The Handler interface declares a method for building the chain of handlers.
  * It also declares a method for executing a request.
  */
-class Handler {
+class Handler { 
  public:
   virtual Handler *SetNext(Handler *handler) = 0;
   virtual std::string Handle(std::string request) = 0;
